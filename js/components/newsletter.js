a713a422875e564e8e1b745bae1310f2b7416d47
Ash Bridges <ashbridges@virginmedia.com>
2022-02-14 18:29:09 +0000

    styled newsletter web component (responsive)

--- js/components/newsletter.js
+++ js/components/newsletter.js
@@ -1,16 +1,20 @@
 class Newsletter extends HTMLElement {
-    connectedCallBack() {
+    connectedCallback() {
         this.innerHTML = `
-        <div class="newsletter__container">
-            <div class="newsletter__header">
-                <h2>Sign up to our newsletter</h2>
-                <p>We'll let you know when our next sale hits</p>
+        <div class="newsletter">
+            <div class="newsletter__container">
+                <div class="newsletter__header">
+                    <h2>Sign up to our newsletter</h2>
+                    <p>We'll let you know when our next sale hits</p>
+                </div>
+                <form class="newsletter__form">
+                    <input type="text" name="email" />
+                    <button class="newsletter__button">Sign up</button>
+                </form>
             </div>
-            <form action="submit" class="newsletter__form">
-                <input type="text" name="email" />
-                <button class="newsletter__button">Sign up</button>
-            </form>
         </div>
-    `
+        `;
     }
 }
+
+window.customElements.define('marquet-newsletter', Newsletter);

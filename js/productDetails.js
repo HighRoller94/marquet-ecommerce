59bbf023f7e686236948aaceb052a29d4adaedfe
Ash Bridges <ashbridges@virginmedia.com>
2022-03-24 21:53:32 +0000

    added reusable product details page using session storage

---
+++ js/productDetails.js
@@ -0,0 +1,33 @@
+const addToCartButtons = document.getElementsByClassName('add__button')
+for (var i = 0; i < addToCartButtons.length; i++) {
+    var button = addToCartButtons[i]
+    button.addEventListener('click', addToCartClicked)
+}
+
+const productInfo = [];
+
+const getProductDetails = () => {
+    // Get product details from session storage
+    const productDetails = JSON.parse(sessionStorage.getItem('productDetails'));
+    // Get the params from the URL (item name), and clean it
+    const paramsNameUncleaned = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&')[0];
+    const paramsName = paramsNameUncleaned.replace(/%20/g, ' ');
+    // Loop over items in productDetails array, if URL params = product name 
+    for (let index = 0; index < productDetails.length; index++) {
+        let product = productDetails[index];
+        if (product.name === paramsName) {
+            productInfo.push(product)
+        }
+    }
+
+    const productName = document.querySelector('.item__name')
+    const productImage = document.querySelector('.item__image')
+    const productPrice = document.querySelector('.item__price')
+
+    productName.innerHTML = productInfo[0].name
+    productImage.src = productInfo[0].image
+    productPrice.innerHTML = productInfo[0].price
+    
+}
+
+getProductDetails()
